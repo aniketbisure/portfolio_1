1c74fe520e31c38ab15f1b597c78df1001b312e4
aniketbisure <155361335+aniketbisure@users.noreply.github.com>
2024-08-16 13:29:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Header from './Components/Header';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Contact from './Components/Contact';
+import Resume from './Components/Resume';
+import Services from './Components/Services';
+import Skill from './Components/Skill';
+
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <About/>
+      <Services/>
+      <Resume/>
+      <Skill/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Resume.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export default function Resume() {
+  return (
+    <>
+    <section class="resume-section" id="resume-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="section-header wow fadeInUp" data-wow-delay=".3s">
+                            <h2 class="section-title">My Education</h2>
+                        </div>
+
+                        <div class="resume-widget">
+                            <div class="resume-item wow fadeInLeft" data-wow-delay=".4s">
+                                <div class="time">
+                                    2024 - Graduation (73.05 %)
+                                </div>
+                                <h3 class="resume-title">Software Development</h3>
+                                <div class="institute">
+                                    Yashwantrao Chavan Institue of Science of Satara,Maharashtra.
+                                </div>
+                            </div>
+                            <div class="resume-item wow fadeInLeft" data-wow-delay=".5s">
+                                <div class="time">
+                                    2021 - HSC (85.50 %)
+                                </div>
+                                <h3 class="resume-title">HSC</h3>
+                                <div class="institute">
+                                    Parents’ Association English School and Junior College of Science, Satara.
+                                </div>
+                            </div>
+                            <div class="resume-item wow fadeInLeft" data-wow-delay=".6s">
+                                <div class="time">
+                                    2019 - SSC (79.40 %)
+                                </div>
+                                <h3 class="resume-title">SSC</h3>
+                                <div class="institute">
+                                Annasaheb Kalyani Vidyalaya ,Satara.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="section-header wow fadeInUp" data-wow-delay=".4s">
+                            <h2 class="section-title">My Internship</h2>
+                        </div>
+
+                        <div class="resume-widget">
+                            <div class="resume-item wow fadeInRight" data-wow-delay=".5s">
+                                <div class="time">
+                                    2023 - 2024
+                                </div>
+                                <h3 class="resume-title"> Php Web Developer</h3>
+                                <div class="institute">
+                                    Yashwantrao Chavan Institue of Science of Satara Maharashtra.<br/>
+                                
+                                </div>
+                            </div>
+                            <div class="resume-item wow fadeInRight" data-wow-delay=".6s">
+                                <div class="time">
+                                    January 1,2024 - February 1,2024
+                                </div>
+                                <h3 class="resume-title">Web Development Internship</h3>
+                                <div class="institute">
+                                    OCTANET SERVICES PVT LTD.
+                                </div>
+                            </div>
+                            <div class="resume-item wow fadeInRight" data-wow-delay=".7s">
+                                <div class="time">
+                                    15 July 2024 - 15 August 2024
+                                </div>
+                                <h3 class="resume-title">ReactJS Developer</h3>
+                                <div class="institute">
+                                  CodSoft PVT LTD.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
